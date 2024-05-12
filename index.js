f825df8bf87609aeb87efb634f5649bc3f66ceef
shahadat <nayemshahadat581@gmail.com>
2024-05-12 14:15:04 +0600

    post route create

--- index.js
+++ index.js
@@ -27,11 +27,27 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-      
+
+        const jobCollection = client.db('jobdb').collection('jobs')
+        app.get('/jobs', async (req, res) => {
+            const result = await jobCollection.find().toArray()
+            res.send(result)
+        })
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await jobCollection.findOne(query);
+            res.send(result)
+        })
+        app.post('/jobs', async (req, res) => {
+            const job = req.body;
+            const result = await jobCollection.insertOne(job);
+            res.send(result);
+        })
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-       
+
     }
 }
 run().catch(console.dir);
